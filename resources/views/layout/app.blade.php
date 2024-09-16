43d1e19a017eec16b8b6c7b63043dd42f28fc8fe
beto <albertocaballero1197@gmail.com>
2024-09-16 00:56:14 -0500

    Cambios

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -6,7 +6,7 @@
         <meta charset="utf-8" />
         <title>@yield('titulo') | {{env('APP_NAME')}}</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
+        <meta content="MEDILAPP - Soluciones digitales del sector salud" name="description" />
         <meta content="Themesbrand" name="author" />
         <!-- App favicon -->
         <link rel="shortcut icon" href="{{asset('images/favicon.ico')}}">
